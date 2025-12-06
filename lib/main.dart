13e2c37eafc25ef5fa350fa014f0c39dba010465
D1225651 <D1225651@o365.fcu.edu.tw>
2025-12-06 22:48:51 +0800

    新增資料庫

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
-import 'auth_page.dart'; // <--- 引入剛剛建立的登入頁檔案
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'auth_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Supabase 初始化
+  await Supabase.initialize(
+    url: 'https://pzyfvhxmugtrrfaraxfi.supabase.co',           // 換成你的 Supabase URL
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6eWZ2aHhtdWd0cnJmYXJheGZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ5MzQzNjcsImV4cCI6MjA4MDUxMDM2N30.5AKQdRxTeS9KBYw98GOIL69MsjN509iyBQic3TyQCHs',                     // 換成你的 anon key
+  );
+
   runApp(const MyApp());
 }
 
@@ -17,7 +26,10 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const AuthPage(), // 設定首頁為登入頁
+      home: const AuthPage(),
     );
   }
 }
+
+// ⚠️ 之後在任何地方要用 Supabase 時用這個：
+final supabase = Supabase.instance.client;
